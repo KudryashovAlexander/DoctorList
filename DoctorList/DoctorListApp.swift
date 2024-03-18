ad735c3645e1e1e4989cbb66ba14211b09a2bef3
KudryashovAlexander <sasha080193@gmail.com>
2024-03-18 12:43:11 +0300

    Initial Commit

---
+++ DoctorList/DoctorListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorListApp.swift
+//  DoctorList
+//
+//  Created by Александр Кудряшов on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DoctorListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
